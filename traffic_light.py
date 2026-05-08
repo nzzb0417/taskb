ff98dd241636ffc644268c6de851b18073c96942
nzzb0417 <nzzb0417@leeds.ac.uk>
2026-05-08 14:04:36 +0100

    Update traffic_light.py

--- traffic_light.py
+++ traffic_light.py
@@ -3,31 +3,32 @@
 # You state that the code submitted is wholly written by yourself. [1]
 # Date: 01/02/2025 [1]
 
-states = { "red":4, "red_amber":3, "green":5, "amber":3 } [1]
+states = {
+    "red": 5,
+    "red_amber": 5,
+    "green": 2,
+    "amber": 4
+}
 
-systime = 0 [1]
-maxtime = int(input("Enter the number of steps that you want to complete: ")) # read an integer number of steps (>0) [1]
+systime = 0
 
-systime = 0 [1, 2]
+maxtime = int(input("Enter the number of steps that you want to complete: "))
 
-state = "red" [1, 2]
+while systime <= maxtime:
+    count = systime % 16
 
+    if count < 5:
+        state = "red"
+    elif count < 10:
+        state = "red_amber"
+    elif count < 12:
+        state = "green"
+    else:
+        state = "amber"
 
-while systime <= maxtime: [1, 2]
-    count = systime % 15 [1, 2]
-    if count >= 0 and count < 4: [1, 2]
-        state = "red" [1, 2]
-        print(f"Time {systime:03} State {state}") [1, 2]
-    elif count >= 4 and count < 7: [1, 2]
-        state = "red_amber" [1, 2]
-        print(f"Time {systime:03} State {state}") [1, 2]
-    elif count >= 7 and count < 12: [1, 2]
-        state = "green" [1, 2]
-        print(f"Time {systime:03} State {state}") [1, 2]
-    elif count >= 12 and count < 15: [2]
-        state = "amber" [2]
-        print(f"Time {systime:03} State {state}") [2]
-    systime = systime + 1 [2]
+    print(f"Time {systime:03} State {state}")
+
+    systime = systime + 1
 
 
 
