d733f25740f2ad398168ce8eccb6b4487ffcdf74
nzzb0417 <nzzb0417@leeds.ac.uk>
2026-05-08 14:02:47 +0100

    Update currency_converter.py

--- currency_converter.py
+++ currency_converter.py
@@ -9,28 +9,22 @@
 ""
 
 def currency_converter(amount, from_currency, to_currency):
-    
+
     conversion_rate = {
         'GBP': {'CNY': 9.24, 'PHP': 75.00, 'INR': 109.06},
         'CNY': {'GBP': 0.11, 'PHP': 8.12, 'INR': 11.81},
         'PHP': {'CNY': 0.12, 'GBP': 0.01, 'INR': 1.45},
         'INR': {'PHP': 0.69, 'CNY': 0.09, 'GBP': 0.01}
     }
-    
-    if from_currency.upper() == "GBP":
-        new_amount = conversion_rate['GBP'][to_currency] * amount
-    elif from_currency.upper() == "CNY":
-        new_amount = conversion_rate['CNY'][to_currency] * amount
-    elif from_currency.upper() == "PHP":
-        new_amount = conversion_rate['PHP'][to_currency] * amount
-    elif from_currency.upper() == "INR":
-        new_amount = conversion_rate['INR'][to_currency] * amount
-    else:
-        print("Error: Please enter one of the currencies listed above")
-        
+
+    from_currency = from_currency.upper()
+    to_currency = to_currency.upper()
+
     if amount < 0:
         return 0.0
-        
+
+    new_amount = conversion_rate[from_currency][to_currency] * amount
+
     return round(new_amount, 2)
 
 
